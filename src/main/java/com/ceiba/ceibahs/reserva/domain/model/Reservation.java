2704d30c93de032d3adad93a5f08ca7b0d457d26
AndrésBlandón <blandonfelipe1@gmail.com>
2021-01-28 16:28:00 -0500

    Se corrige ubicación de métodos de validación de reserva Pasan del servicio al modelo

--- src/main/java/com/ceiba/ceibahs/reserva/domain/model/Reservation.java
+++ src/main/java/com/ceiba/ceibahs/reserva/domain/model/Reservation.java
@@ -4,9 +4,14 @@ import com.ceiba.ceibahs.analista.domain.model.Analyst;
 import com.ceiba.ceibahs.utils.FieldValidate;
 import com.ceiba.ceibahs.utils.enums.PaymentType;
 import com.ceiba.ceibahs.utils.enums.ReservationStatus;
+import com.ceiba.ceibahs.utils.exception.InvalidReservationHourException;
+import com.ceiba.ceibahs.utils.exception.NoValidReservationValueException;
+import com.ceiba.ceibahs.utils.exception.ReservationDayNotValidException;
 import lombok.Getter;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 public class Reservation {
@@ -15,6 +20,10 @@ public class Reservation {
     private static final String VALUE_OBLIGATORY = "El valor de reserva es obligatorio";
     private static final String ANALYST_OBLIGATORY = "La reserva debe tener un analista seleccionado";
     private static final String PAYMENT_TYPE_OBLIGATORY = "Debe seleccionar un tipo de pago";
+    private static final String INVALID_RESERVATION_VALUE = "El valor de la reserva no ha podido ser calculado correctamente";
+    private static final String RESERVATION_DAY_NOT_VALID = "No es posible programar una reserva para un día domingo";
+    private static final String INVALID_RESERVATION_DATE = "No es posible programar una reserva para un día y hora anterior al actual";
+    private static final String INVALID_RESERVATION_HOUR = "La hora seleccionada para la reserva no es válida";
 
     private final Long id;
     private final Analyst analyst;
@@ -33,6 +42,10 @@ public class Reservation {
         FieldValidate.validateNotNull(analyst, ANALYST_OBLIGATORY);
         FieldValidate.validateNotNull(paymentType, PAYMENT_TYPE_OBLIGATORY);
 
+        validateThatTheReservationDayIsNotSunday(reservationDate);
+        validateInvalidHours(reservationDate);
+        validateReservationValue(value, reservationDate);
+
         this.id = id;
         this.analyst = analyst;
         this.reservationDate = reservationDate;
@@ -48,5 +61,51 @@ public class Reservation {
 
     public void setValue(int value) {
         this.value = value;
+    }
+
+    public int calculateMoneyChange(int value, int dollarValue) {
+        double result = (double) value /  (double) dollarValue;
+        return (int) Math.ceil(result);
+    }
+
+    /**
+     * Nos permite validar que no seleccionemos fecha y hora anteriores a la actual.
+     * @param localDateTime
+     */
+    public void validatePreviousDays(LocalDateTime localDateTime) {
+        LocalDateTime currentLocalDateTime = LocalDateTime.now().plusMinutes(30);
+        if (localDateTime.isBefore(currentLocalDateTime)) {
+            throw new InvalidReservationHourException(INVALID_RESERVATION_DATE);
+        }
+    }
+
+    public void validateThatTheReservationDayIsNotSunday(LocalDateTime reservationDate) {
+        int dayOfTheWeek = reservationDate.getDayOfWeek().getValue();
+        if (dayOfTheWeek == DayOfWeek.SUNDAY.getValue()) {
+            throw new ReservationDayNotValidException(RESERVATION_DAY_NOT_VALID);
+        }
+    }
+
+    public void validateInvalidHours(LocalDateTime reservationDate) {
+        List<Integer> weekValidHours = List.of(9, 10, 11, 13, 14, 15, 16, 17);
+        List<Integer> saturdayValidHours = List.of(9, 10, 11, 13, 14);
+        int dayOfTheWeek = reservationDate.getDayOfWeek().getValue();
+        if ((dayOfTheWeek == DayOfWeek.SATURDAY.getValue() && !saturdayValidHours.contains(reservationDate.getHour()))
+                || (dayOfTheWeek != DayOfWeek.SUNDAY.getValue() && !weekValidHours.contains(reservationDate.getHour()))) {
+            throw new InvalidReservationHourException(INVALID_RESERVATION_HOUR);
+        }
+    }
+
+    public void validateReservationValue(int value, LocalDateTime reservationDate){
+        int dayOfTheWeek = reservationDate.getDayOfWeek().getValue();
+        if (dayOfTheWeek == DayOfWeek.SATURDAY.getValue()) {
+            if (value != 40000) {
+                throw new NoValidReservationValueException(INVALID_RESERVATION_VALUE);
+            }
+        } else {
+            if (value != 35000) {
+                throw new NoValidReservationValueException(INVALID_RESERVATION_VALUE);
+            }
+        }
     }
 }
--- src/main/java/com/ceiba/ceibahs/reserva/domain/service/CreateReservationService.java
+++ src/main/java/com/ceiba/ceibahs/reserva/domain/service/CreateReservationService.java
@@ -5,20 +5,8 @@ import com.ceiba.ceibahs.reserva.domain.model.ReservationDto;
 import com.ceiba.ceibahs.reserva.domain.port.ReservationRepository;
 import com.ceiba.ceibahs.utils.enums.PaymentType;
 import com.ceiba.ceibahs.utils.enums.ReservationStatus;
-import com.ceiba.ceibahs.utils.exception.InvalidReservationHourException;
-import com.ceiba.ceibahs.utils.exception.NoValidReservationValueException;
-import com.ceiba.ceibahs.utils.exception.ReservationDayNotValidException;
-
-import java.time.DayOfWeek;
-import java.time.LocalDateTime;
-import java.util.List;
 
 public class CreateReservationService {
-
-    private static final String INVALID_RESERVATION_VALUE = "El valor de la reserva no ha podido ser calculado correctamente";
-    private static final String RESERVATION_DAY_NOT_VALID = "No es posible programar una reserva para un día domingo";
-    private static final String INVALID_RESERVATION_DATE = "No es posible programar una reserva para un día y hora anterior al actual";
-    private static final String INVALID_RESERVATION_HOUR = "La hora seleccionada para la reserva no es válida";
 
     private final ReservationRepository reservationRepository;
 
@@ -27,74 +15,12 @@ public class CreateReservationService {
     }
 
     public ReservationDto create(Reservation reservation) {
-        validateDayOfReservation(reservation.getReservationDate());
-        validateInvalidHours(reservation.getReservationDate());
-        validatePreviousDays(reservation.getReservationDate());
-        validateReservationValue(reservation.getValue(), reservation.getReservationDate());
+        reservation.validatePreviousDays(reservation.getReservationDate());
         reservation.setStatus(ReservationStatus.ACTIVE);
+
         if (reservation.getPaymentType().equals(PaymentType.USD)) {
-            reservation.setValue(calculateMoneyChange(reservation.getValue(), reservation.getDollarValue()));
+            reservation.setValue(reservation.calculateMoneyChange(reservation.getValue(), reservation.getDollarValue()));
         }
         return reservationRepository.create(reservation);
-    }
-
-    private int calculateMoneyChange(int value, int dollarValue) {
-        double result = (double) value /  (double) dollarValue;
-        return (int) Math.ceil(result);
-    }
-
-    /**
-     * Nos permite validar que no seleccionemos fecha y hora anteriores a la actual.
-     * @param localDateTime
-     */
-    private void validatePreviousDays(LocalDateTime localDateTime) {
-        LocalDateTime currentLocalDateTime = LocalDateTime.now().plusMinutes(30);
-        if (localDateTime.isBefore(currentLocalDateTime)) {
-            throw new InvalidReservationHourException(INVALID_RESERVATION_DATE);
-        }
-    }
-
-    /**
-     * Nos permite validar que el día seleccionado para la reserva no sea domingo
-     * @param reservationDate
-     */
-    private void validateDayOfReservation(LocalDateTime reservationDate) {
-        int dayOfTheWeek = reservationDate.getDayOfWeek().getValue();
-        if (dayOfTheWeek == DayOfWeek.SUNDAY.getValue()) {
-            throw new ReservationDayNotValidException(RESERVATION_DAY_NOT_VALID);
-        }
-    }
-
-    /**
-     * Nos permite verificar que las horas seleccionadas para la reserva concuerden con la disponibilidad
-     * de horas establecido
-     * @param reservationDate
-     */
-    private void validateInvalidHours(LocalDateTime reservationDate) {
-        List<Integer> weekValidHours = List.of(9, 10, 11, 13, 14, 15, 16, 17);
-        List<Integer> saturdayValidHours = List.of(9, 10, 11, 13, 14);
-        int dayOfTheWeek = reservationDate.getDayOfWeek().getValue();
-        if ((dayOfTheWeek == DayOfWeek.SATURDAY.getValue() && !saturdayValidHours.contains(reservationDate.getHour()))
-                || (dayOfTheWeek != DayOfWeek.SUNDAY.getValue() && !weekValidHours.contains(reservationDate.getHour()))) {
-            throw new InvalidReservationHourException(INVALID_RESERVATION_HOUR);
-        }
-    }
-
-    /**
-     * Nos permite validar el valor de la reserva donde se busca evitar recibir precios no establecidos
-     * @param value
-     * @param reservationDate
-     */
-    private void validateReservationValue(int value, LocalDateTime reservationDate){
-        int dayOfTheWeek = reservationDate.getDayOfWeek().getValue();
-        if (dayOfTheWeek == DayOfWeek.SATURDAY.getValue()) {
-            if (value != 40000) {
-                throw new NoValidReservationValueException(INVALID_RESERVATION_VALUE);
-            }
-        } else {
-            if (value != 35000) {
-                throw new NoValidReservationValueException(INVALID_RESERVATION_VALUE);
-            }
-        }
     }
 }
